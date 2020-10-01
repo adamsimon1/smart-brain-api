1e09175488354fe504dd6181a5c055d3354bd506
adamsimon1 <50641162+adamsimon1@users.noreply.github.com>
2020-09-30 20:37:14 -0400

    server connected

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
     {
       id: '123',
       name: 'John',
-      email: 'john@gmail.com',
       password: 'cookies',
+      email: 'john@gmail.com',
       entries: 0,
       joined: new Date()
     },
     {
       id: '124',
       name: 'Sally',
-      email: 'sally@gmail.com',
       password: 'bananas',
+      email: 'sally@gmail.com',
       entries: 0,
       joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: 987,
+      hash: '',
+      email: 'john@gmail.com'
     }
   ]
 }
@@ -45,8 +55,12 @@ app.get('/profile/:id', (req, res, next) => {
 })
 
 app.post('/signin', (req, res) => {
+  bcrypt.compare('psw', '$2a$10$s/ODAiM3KhFVTKtu0IEoWekp6F5gYCrmA7TniSkuzUGT0DLK32PYy', function(err, res) {
+    console.log(res)
+  });
+
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json('success')
+    res.json(database.users[0])
   } else {
     res.status(400).json('error logging in')
   }
@@ -54,12 +68,14 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const {email, name, password} = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash)
+  });
   database.users.push(
     {
       id: '125',
       name: name,
       email: email,
-      password: password,
       entries: 0,
       joined: new Date()
     }
@@ -78,8 +94,10 @@ app.put('/image', (req, res) => {
     }
   })
   if (!found) {
-    res.status(404).json('no such user');
+    res.status(404).json('not found');
   }
 })
+
+
 
 app.listen(3000, () => console.log('listening at http://localhost:3000'))
